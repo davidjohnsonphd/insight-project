7f52adda60e4cbda07fce8b2df97bd57a7c9cbe8
David Johnson <davidjjohnsonphd@gmail.com>
2020-06-16 17:16:31 -0500

    Updated dashboard for deployment to AWS

--- dashboard.py
+++ dashboard.py
@@ -191,6 +191,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 campaigns = df['Campaign'].unique()
 
 # Produces the figure, table, text, and buttons
